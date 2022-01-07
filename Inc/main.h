607d51fbcb9988854633cf20a050a2784cc620c4
Szymon <87312593+szymon9997@users.noreply.github.com>
2022-01-07 16:01:18 +0100

    Add files via upload

---
+++ Inc/main.h
@@ -0,0 +1,143 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f3xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define DRDY_Pin GPIO_PIN_2
+#define DRDY_GPIO_Port GPIOE
+#define CS_I2C_SPI_Pin GPIO_PIN_3
+#define CS_I2C_SPI_GPIO_Port GPIOE
+#define MEMS_INT3_Pin GPIO_PIN_4
+#define MEMS_INT3_GPIO_Port GPIOE
+#define MEMS_INT4_Pin GPIO_PIN_5
+#define MEMS_INT4_GPIO_Port GPIOE
+#define OSC32_IN_Pin GPIO_PIN_14
+#define OSC32_IN_GPIO_Port GPIOC
+#define OSC32_OUT_Pin GPIO_PIN_15
+#define OSC32_OUT_GPIO_Port GPIOC
+#define OSC_IN_Pin GPIO_PIN_0
+#define OSC_IN_GPIO_Port GPIOF
+#define OSC_OUT_Pin GPIO_PIN_1
+#define OSC_OUT_GPIO_Port GPIOF
+#define SPI1_SCK_Pin GPIO_PIN_5
+#define SPI1_SCK_GPIO_Port GPIOA
+#define SPI1_MISO_Pin GPIO_PIN_6
+#define SPI1_MISO_GPIO_Port GPIOA
+#define SPI1_MISOA7_Pin GPIO_PIN_7
+#define SPI1_MISOA7_GPIO_Port GPIOA
+#define STBY_PRAWY_Pin GPIO_PIN_8
+#define STBY_PRAWY_GPIO_Port GPIOE
+#define IN2_LEWY_Pin GPIO_PIN_11
+#define IN2_LEWY_GPIO_Port GPIOE
+#define IN2_PRAWY_Pin GPIO_PIN_12
+#define IN2_PRAWY_GPIO_Port GPIOE
+#define IN1_LEWY_Pin GPIO_PIN_13
+#define IN1_LEWY_GPIO_Port GPIOE
+#define IN1_PRAWY_Pin GPIO_PIN_14
+#define IN1_PRAWY_GPIO_Port GPIOE
+#define STBY_LEWY_Pin GPIO_PIN_15
+#define STBY_LEWY_GPIO_Port GPIOE
+#define PWM_PRAWY_Pin GPIO_PIN_12
+#define PWM_PRAWY_GPIO_Port GPIOD
+#define DM_Pin GPIO_PIN_11
+#define DM_GPIO_Port GPIOA
+#define DP_Pin GPIO_PIN_12
+#define DP_GPIO_Port GPIOA
+#define SWDIO_Pin GPIO_PIN_13
+#define SWDIO_GPIO_Port GPIOA
+#define SWCLK_Pin GPIO_PIN_14
+#define SWCLK_GPIO_Port GPIOA
+#define PWM_LEWY_Pin GPIO_PIN_15
+#define PWM_LEWY_GPIO_Port GPIOA
+#define czujnik0_Pin GPIO_PIN_0
+#define czujnik0_GPIO_Port GPIOD
+#define czujnik1_Pin GPIO_PIN_1
+#define czujnik1_GPIO_Port GPIOD
+#define czujnik2_Pin GPIO_PIN_2
+#define czujnik2_GPIO_Port GPIOD
+#define czujnik3_Pin GPIO_PIN_3
+#define czujnik3_GPIO_Port GPIOD
+#define czujnik4_Pin GPIO_PIN_4
+#define czujnik4_GPIO_Port GPIOD
+#define czujnik5_Pin GPIO_PIN_5
+#define czujnik5_GPIO_Port GPIOD
+#define czujnik6_Pin GPIO_PIN_6
+#define czujnik6_GPIO_Port GPIOD
+#define czujnik7_Pin GPIO_PIN_7
+#define czujnik7_GPIO_Port GPIOD
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+#define I2C1_SCL_Pin GPIO_PIN_6
+#define I2C1_SCL_GPIO_Port GPIOB
+#define I2C1_SDA_Pin GPIO_PIN_7
+#define I2C1_SDA_GPIO_Port GPIOB
+#define MEMS_INT2_Pin GPIO_PIN_1
+#define MEMS_INT2_GPIO_Port GPIOE
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
